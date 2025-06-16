77eb6ecfefdd2e1eaa36e1ce9d516f2e280e745e
Aniketyadav05 <yadavaniket7611@example.com>
2025-06-16 23:08:00 +0530

    papers defined

--- src/pages/AllPapers.tsx
+++ src/pages/AllPapers.tsx
@@ -2,7 +2,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchAllPapers } from "../services/conf";
 import { Link } from "react-router";
-
+type Paper = {
+  id: string;
+  subject: string;
+  Year: string;
+  Degree: string;
+  University: string;
+};
 const PapersList = () => {
   const { data: papers, isLoading, isError } = useQuery({
     queryKey: ["papers"],
@@ -16,7 +22,7 @@ const PapersList = () => {
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">📚 All Generated Papers</h1>
       <ul className="space-y-4">
-        {papers.map((paper: any) => (
+        {papers && papers.slice(0, 4).map((paper: Paper) => (
           <li key={paper.id} className="border p-4 rounded shadow">
             <h2 className="font-semibold text-lg text-white">{paper.subject} ({paper.Year})</h2>
             <p>{paper.Degree}</p>
